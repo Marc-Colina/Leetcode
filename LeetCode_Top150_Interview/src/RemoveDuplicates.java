21b1f4d2f32d1a52bd84dd8534df9fdb04e898ca
Marc-Colina <marccolina456@gmail.com>
2024-03-29 21:02:18 -0500

    Added comments to RemoveDuplicates

--- LeetCode_Top150_Interview/src/RemoveDuplicates.java
+++ LeetCode_Top150_Interview/src/RemoveDuplicates.java
@@ -35,7 +35,7 @@
 //It does not matter what you leave beyond the returned k (hence they are underscores).
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int [] nums = {0,1,1,2,2,3,3,3,4,5,5,5,6,6,7};
+        int [] nums = {0,1,1,1,2,2,3,3,3,4,5,5,5,6,6,7};
         System.out.println(removeDuplicates(nums));
         for(int num : nums)
         {
@@ -45,16 +45,31 @@ public class RemoveDuplicates {
 
     public static int removeDuplicates(int[] nums)
     {
+        //k in this program will determine how many unique elements are present in our array.
         int k =0;
+
+        //We want to return early so if the array is either empty or only has one element, we just return the length
+        //right away.
         if(nums.length<2)
             return nums.length;
 
-        for(int counter=0; counter<nums.length;counter++)
+        for(int counter=0; counter<nums.length; counter++)
         {
-            if(counter<nums.length-1 && nums[k] != nums[counter+1])
+            //Since our array is sorted in ascending order, we can go ahead and do a side by side comparison per element
+            //and we are sure that once we move k to the right, we will not encounter any other element that is smaller
+            //than the value at index k.
+            //The trick here is the value of k. On the first iteration, k will be 0 so it will compare the first element
+            //to the other elements. As it traverses the array, everytime the counter index has a value that's the same as the
+            //value of the k index, the k index will not be incremented. While continuing the traversal, when the counter index now has
+            // an element different from the element at index k, we then increment k and put the element in the counter index to
+            //the now incremented index k. At the end of the iteration,all the unique elements have been added to the beginning of
+            //the array thanks to the k variable that kept track of it. The remaining elements to the right of the k index,
+            //are not significant and these are elements that are expected to be duplicates of the unique elements from the 0-th
+            //to the k-th index.
+            if(nums[k] != nums[counter])
             {
                 k++;
-                nums[k] = nums[counter+1];
+                nums[k] = nums[counter];
             }
         }
         return ++k;
