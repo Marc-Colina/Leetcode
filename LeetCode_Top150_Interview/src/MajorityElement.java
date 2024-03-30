80063e0219a1f2576a98be586e46492c3fdc1644
Marc-Colina <marccolina456@gmail.com>
2024-03-29 20:13:57 -0500

    Add comments to Majority Element solutions

--- LeetCode_Top150_Interview/src/MajorityElement.java
+++ LeetCode_Top150_Interview/src/MajorityElement.java
@@ -2,7 +2,8 @@ import java.util.HashMap;
 
 //Given an array nums of size n, return the majority element.
 //
-//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always
+// exists in the array.
 //
 //
 //
@@ -23,6 +24,11 @@ public class MajorityElement {
 
     public static int majorityElement(int[] nums)
     {
+        //In this approach, we are adding to the count everytime we see a number that's the same
+        //with the number in the candidate, and we subtract the count when we see a different number.
+        //We will only be changing the candidate once the count is equal to 0. Through this approach,
+        //once we finished traversing the array, the element with the most occurrence (which is the majority element
+        // assuming that there always will be one) will remain as candidates value.
        int count = 0;
        int candidate = 0;
 
@@ -44,27 +50,36 @@ public class MajorityElement {
        return candidate;
     }
 
-//    public static int majorityElement(int[] nums)
-//    {
-//        int majorityElement = 0;
-//        HashMap<Integer, Integer>  map = new HashMap<>();
-//        for(int number : nums)
-//        {
-//            if(map.containsKey(number))
-//            {
-//                int currentCount = map.get(number);
-//                map.put(number, ++currentCount);
-//            }
-//            else{
-//                map.put(number,1);
-//            }
-//        }
-//
-//        for(int key: map.keySet())
-//        {
-//            if(map.get(key) > nums.length/2)
-//                majorityElement = key;
-//        }
-//        return majorityElement;
-//    }
+    //This is another approach utilizing a HashMap. We traverse the nums array and put them in a HashMap.
+    public static int majorityElementHashMap(int[] nums)
+    {
+        int majorityElement = 0;
+        HashMap<Integer, Integer>  map = new HashMap<>();
+        for(int number : nums)
+        {
+            //We will use the numbers in the array as key and the value as the number of occurrences in the array.
+            // If the map already contains the number, we fetch the current value and add one to it.
+            if(map.containsKey(number))
+            {
+                int currentCount = map.get(number);
+                map.put(number, ++currentCount);
+            }
+            //If the map does not yet contain the number, we put the number and put a value of 1 which means it's the
+            //first time we are encountering that number.
+            else{
+                map.put(number,1);
+            }
+        }
+
+        //By the end of the iteration, we would already have all the numbers with the number of occurrences in our HashMap.
+        //We then loop through our keys, get the value and check if it is greater than nums.length/2 (which is the formula
+        //that qualifies a majority element). Since there can't be more than one whose value is greater than nums.length/2,
+        //we are able to fetch our majority element.
+        for(int key: map.keySet())
+        {
+            if(map.get(key) > nums.length/2)
+                majorityElement = key;
+        }
+        return majorityElement;
+    }
 }
